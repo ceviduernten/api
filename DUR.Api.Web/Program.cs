480dcdc247c9a8735634235fcc6d386de19e2971
Urs Forrer <mail@ursforrer.ch>
2020-01-01 20:25:15 +0100

    Final adjustments for the succesfull deployment

--- DUR.Api.Web/Program.cs
+++ DUR.Api.Web/Program.cs
@@ -1,6 +1,11 @@
+using System;
 using Autofac.Extensions.DependencyInjection;
+using DUR.Api.Repo.Database;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace DUR.Api.Web
 {
--- DUR.Api.Web/Startup.cs
+++ DUR.Api.Web/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -55,6 +56,7 @@ namespace DUR.Api.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -75,6 +77,17 @@ namespace DUR.Api.Web
             MapperInjector.RegisterModule(builder);
             NextcloudRepositoryInjector.RegisterModule(builder);
             DatabaseRepositoryInjector.RegisterModule(builder);
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<RepositoryContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
         }
     }
 }
