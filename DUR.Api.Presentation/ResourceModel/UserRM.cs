ec8285768fde57936dbda0b1f047a288d22e34db
Urs Forrer <mail@ursforrer.ch>
2020-05-02 13:17:10 +0200

    Added userm for the application

--- DUR.Api.Presentation/ResourceModel/UserRM.cs
+++ DUR.Api.Presentation/ResourceModel/UserRM.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using DUR.Api.Entities.Admin;
 
 namespace DUR.Api.Presentation.ResourceModel
 {
     public class UserRM : BaseRM
     {
-        
+        public Guid IdUser { get; set; }
+        public string LoginName { get; set; }
+        public string FullName { get; set; }
+        public string Vulgo { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public Role Role { get; set; }
+        public string RoleString
+        {
+            get
+            {
+                return Role.ToString();
+            }
+        }
     }
 }
