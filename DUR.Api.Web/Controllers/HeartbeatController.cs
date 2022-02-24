e8eeca71ee88b4f1f282fdf9cd117836c7de37a5
Urs Forrer <mail@ursforrer.ch>
2022-02-24 21:05:42 +0100

    Remove unused code

--- DUR.Api.Web/Controllers/HeartbeatController.cs
+++ DUR.Api.Web/Controllers/HeartbeatController.cs
@@ -1,7 +1,5 @@
 ﻿using DUR.Api.Web.Default;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net.Http;
 
 namespace DUR.Api.Web.Controllers
 {
@@ -16,19 +14,6 @@ namespace DUR.Api.Web.Controllers
         public JsonResult Current()
         {
             return Json(new InfoJsonResult(200, "System Heartbeat oke"));
-        }
-
-        [HttpGet("Test")]
-        public JsonResult Test()
-        {
-            string url = "https://***REMOVED***/remote.php/dav/public-calendars/***REMOVED***?export&accept=jcal";
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new System.Uri(url);
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            var result = response.Content.ReadAsStringAsync().Result;
-            var s = JsonConvert.DeserializeObject(result);
-            return null;
         }
     }
 }
